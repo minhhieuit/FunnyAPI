843f9a1e45ffb2d4a9d4a14ce2d3b4b32c4a607e
Jacky Nguyen <info.hieuit@gmail.com>
2020-09-09 14:49:40 +0700

    Add project files.

---
+++ FunnyAPI/AuthorizedTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunnyAPI
+{
+    public class AuthorizedTest : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            return;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var descriptor = context.ActionDescriptor;
+            var isLogin = ((ControllerBase)context.Controller).User.Identity.IsAuthenticated;
+            return;
+        }
+    }
+}
